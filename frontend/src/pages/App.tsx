f8beccf1a6429d57670411deb92e33869fa8c87e
Elias-Larsson <aroezi05@gmail.com>
2025-09-01 13:37:58 +0200

    feat: add react router

--- frontend/src/pages/App.tsx
+++ frontend/src/pages/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
-
-function App() {
+import { Link } from 'react-router-dom';
+export function App() {
+  
 const [users, setUsers] = useState<string[]>([]);
 useEffect(() =>{
   const userarray = ["Jonas", "Nina", "Ahmed"]
@@ -11,9 +12,14 @@ useEffect(() =>{
     <div className="flex flex-col justify-center items-center gap-10 h-screen">
       <h1 className="text-2xl underline">Spin the wheel or die?</h1>
       {users.map((user) => (
-      <button className="button">Purchase item from {user}</button>
+        <div className="flex flex-col">
+          <button className="button">Purchase item from {user}</button>
+          <Link className="link" to="/profiles">log in as {user}</Link>
+        </div>
         ))}
+
     </div>
+
   )
 }
 
---
+++ frontend/src/routes.tsx
@@ -0,0 +1,13 @@
+import { createBrowserRouter } from "react-router";
+import { Profile } from "./pages/profile.tsx"
+import { App } from "./pages/App.tsx"
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/profile",
+    element: <  Profile />  ,
+  },
+]);
