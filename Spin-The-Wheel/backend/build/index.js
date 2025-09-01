cb5851ff3d1ae82ac565b2d2723d88b7d283c931
David Dyberg <david.dyberg@gmail.com>
2025-09-01 10:13:15 +0200

    setup fastify

---
+++ Spin-The-Wheel/backend/build/index.js
@@ -0,0 +1,15 @@
+import Fastify from "fastify";
+const fastify = Fastify({
+    logger: true,
+});
+const port = 3000;
+fastify.get("/", function (request, reply) {
+    reply.send("Hello Ida");
+});
+fastify.listen({ port: port }, function (err, address) {
+    if (err) {
+        fastify.log.error(err);
+        process.exit(1);
+    }
+});
+//# sourceMappingURL=index.js.map
