a4e99d1599c61c3bd47885258237ded0db801e07
Chokladglasyr <lim_ida@live.com>
2025-09-01 11:14:24 +0200

    feat: added ordermodel with interface and schema. refactor: added created at to usermodel

--- backend/src/models/userModel.ts
+++ backend/src/models/userModel.ts
@@ -6,14 +6,16 @@ export interface IUser extends Document {
     spins: number;
     rewards: string[];
     reward: string;
+    created_at: string;
 
 }
 const userSchema: Schema = new Schema ({
     name: {type: String, required:true},
     orders:[{type: String}],
-    spins: {type: Number},
+    spins: {type: Number, default: 0},
     rewards: [{type: String}],
     reward: {type: String},
+    created_at: {type: String, default: () => new Date().toISOString() }
 
 })
 export const User = mongoose.model<IUser>("User", userSchema)
