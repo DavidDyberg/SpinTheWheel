c5c442c75dbfec763e56b4a7625aa3bb748a481c
Elias-Larsson <aroezi05@gmail.com>
2025-09-01 11:03:46 +0200

    feat: add map function for user array

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,18 @@
-import './App.css'
+import { useEffect, useState } from 'react'
 
 function App() {
+const [users, setUsers] = useState<string[]>([]);
+useEffect(() =>{
+  const userarray = ["Jonas", "Nina", "Ahmed"]
+  setUsers(userarray)
+}, [])
+
   return (
     <div className="flex flex-col justify-center items-center gap-10 h-screen">
       <h1 className="text-2xl underline">Spin the wheel or die?</h1>
-      <button className="button">Purchase item from Jonas</button>
-      <button className="button">Purchase item from Lars</button>
-      <button className="button">Purchase item from Ahmed</button>
+      {users.map((user) => (
+      <button className="button">Purchase item from {user}</button>
+        ))}
     </div>
   )
 }
