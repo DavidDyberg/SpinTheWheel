c981893d1748bb8e893b53c75218d57196a2ba40
Chokladglasyr <lim_ida@live.com>
2025-09-01 21:51:44 +0200

    fix: had to add an alternative db connection due to timeout

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,8 @@
 import Fastify from "fastify";
 import fastifyMongo from "@fastify/mongodb";
 import dotenv from "dotenv";
+import { getUsers } from "../build/controllers/userController.js";
+import { connectDB } from "../build/db.js";
 
 dotenv.config();
 
@@ -10,23 +12,27 @@ const fastify = Fastify({
 
 const port = 3000;
 
-fastify.register(fastifyMongo, {
-  forceClose: true,
-  url: process.env.MONGODB_URL as string,
-  database: "spin_the_wheel",
-});
+// fastify.register(fastifyMongo, {
+//   forceClose: true,
+//   url: process.env.MONGODB_URL as string,
+//   database: "spin_the_wheel",
+// });
 
-fastify.ready((err) => {
-  if (fastify.mongo.db) {
-    console.log(`Connected to MongoDB: ${fastify.mongo.db.databaseName}`);
-  } else {
-    console.error("Failed to connect to MongoDB", err);
-  }
-});
+// fastify.ready((err) => {
+//   if (fastify.mongo.db) {
+//     console.log(`Connected to MongoDB: ${fastify.mongo.db.databaseName}`);
+//   } else {
+//     console.error("Failed to connect to MongoDB", err);
+//   }
+// });
+
+connectDB()
 
 fastify.get("/", function (req, reply) {
   reply.send("Hello Ida");
 });
+
+fastify.get('/users', getUsers)
 
 fastify.listen({ port: port }, function (err, address) {
   if (err) {
