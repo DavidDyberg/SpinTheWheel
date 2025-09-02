2a1f6a2659bb20c568c759467421a956a08bcee6
Chokladglasyr <lim_ida@live.com>
2025-09-02 10:59:21 +0200

    refactor: changed res.code to res.status in usercontroller

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -4,7 +4,7 @@ import { User } from "../models/userModel.js"
 export const getUsers = async (req: FastifyRequest, res: FastifyReply) => {
     const users = await User.find();
     if(!users || users.length === 0) {
-        res.code(404).send({message: `Can't find any users.`})
+        res.status(404).send({message: `Can't find any users.`})
     }
     res.send(users)
 }
