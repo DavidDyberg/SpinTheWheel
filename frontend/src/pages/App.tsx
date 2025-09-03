3eec7b883febfcec19323be3301b51c7aa877a56
Elias-Larsson <aroezi05@gmail.com>
2025-09-03 10:46:25 +0200

    feat: add fetchusers function

--- frontend/src/pages/App.tsx
+++ frontend/src/pages/App.tsx
@@ -1,25 +1,33 @@
-import { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 export function App() {
-  
-const [users, setUsers] = useState<string[]>([]);
-useEffect(() =>{
-  const userarray = ["Jonas", "Nina", "Ahmed"]
-  setUsers(userarray)
-}, [])
+  const [users, setUsers] = useState<string[]>([]);
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const res = await fetch("http://[::1]:3000/users");
+        const data = await res.json();
+        console.log(data);
+        setUsers(data);
+      } catch (err: unknown) {
+        console.error("failed to fetch user data:", err);
+      }
+    };
+    fetchUsers();
+  }, []);
   return (
     <div className="main">
       <h1 className="text-2xl underline">Spin the wheel or die?</h1>
       {users.map((user) => (
         <div className="flex flex-col">
           <button className="button">Purchase item from {user}</button>
-          <Link className="link" to={`/profile/${user}`}>log in as {user}</Link>
+          <Link className="link" to={`/profile/${user}`}>
+            log in as {user}
+          </Link>
         </div>
-        ))}
-
+      ))}
     </div>
-
-  )
+  );
 }
 
-export default App
+export default App;
