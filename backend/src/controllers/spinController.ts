a1a0f91a09aba55850325d4dcc36197726e7d5d5
Chokladglasyr <lim_ida@live.com>
2025-09-03 14:17:53 +0200

    refactor: hardcoded rewards

--- backend/src/controllers/spinController.ts
+++ backend/src/controllers/spinController.ts
@@ -4,12 +4,6 @@ import { Rewards } from "../models/rewardsModel";
 
 export const spinAction = async (req: FastifyRequest, res: FastifyReply) => {
 
-    const reward_one = '50/100'
-    const reward_two = '20/100'
-    const reward_three = '15/100'
-    const reward_four = '10/10'
-    const reward_five = '5/100'
-
     const { user_id } = req.params as {user_id: string}
     console.log(user_id)
     let userReward =''
@@ -18,19 +12,19 @@ export const spinAction = async (req: FastifyRequest, res: FastifyReply) => {
     console.log(num)
     switch (true) {
         case num <= 50:
-            userReward = reward_one
+            userReward = "Triss-lott"
             break;
         case num <= 70:
-            userReward = reward_two
+            userReward = "Biobiljett"
             break;
         case num <= 85:
-            userReward = reward_three
+            userReward = "Macbook Pro"
             break
         case num <= 95:
-            userReward = reward_four
+            userReward = "Bali resa"
             break
         case num <= 100:
-            userReward = reward_five
+            userReward = "Yacht"
             break
     }
 
