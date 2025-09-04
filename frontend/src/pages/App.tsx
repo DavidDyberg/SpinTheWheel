d306bd5807ed7983d30662081cbc5430a545d47f
Chokladglasyr <lim_ida@live.com>
2025-09-04 10:46:30 +0200

    feat: added so purchase fetches createorder and show a msg for 3sec

--- frontend/src/pages/App.tsx
+++ frontend/src/pages/App.tsx
@@ -4,6 +4,7 @@ import type { User } from "../type";
 
 export const App = () => {
   const [users, setUsers] = useState<User[]>([]);
+  const [msg, setMsg] = useState('')
   useEffect(() => {
     const fetchUsers = async () => {
       try {
@@ -17,7 +18,24 @@ export const App = () => {
     };
     fetchUsers();
   }, []);
-
+  const makePurchase = async (id: string) => {
+    try {
+      const res = await fetch(`http://[::1]:3000/user?user_id=${id}`, {
+        credentials: "include",
+        method: 'POST',
+      })
+      const data = await res.json()
+      setMsg(data.message)
+      setTimeout(() => {
+        document.getElementById("message")!.style.display = "none"
+      }, 3000)
+    } catch(err: unknown) {
+      if(err instanceof Error) {
+        console.error("Failed to make a purchase ", err)
+        return;
+      }
+    }
+  }
 
   return (
     <div className="main">
@@ -25,12 +43,13 @@ export const App = () => {
       {users.map((user, index) => (
 
         <div className="flex flex-col" key={index}>
-          <button className="button">Purchase item from {user.name}</button>
+          <button onClick={() => makePurchase(user._id)} className="button">Purchase item from {user.name}</button>
           <Link className="link" to={`/profile/${user._id}`}>
             log in as {user.name}
           </Link>
         </div>
       ))}
+      <p id="message">{msg}</p>
     </div>
   );
 }
